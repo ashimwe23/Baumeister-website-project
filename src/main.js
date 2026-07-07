a595a9055b9e8b7842128562d0527f0c03c90099
Belyse Ashimwe <ashimwebelyse233@gmail.com>
2026-07-07 14:54:39 +0200

    Updated the Hero section y adding the animation

--- src/main.js
+++ src/main.js
@@ -10,6 +10,47 @@ const tl = gsap.timeline({
   },
 });
 
+tl.to("#pink", {
+  y: -19,
+  duration: 1,
+}, "<");
+
+
+tl.to("#green2", {
+  y: -190,
+  duration: 0.6,
+},"<");
+
+tl.to(["#blue","#green","#red"], {
+  y: -60,
+  duration: 1,
+}, "<");
+
+tl.to(["#red", "#green2"], {
+  y: -370,
+ duration: 1,
+}), "<";
+
+
+tl.to("#green", {
+  y: -105,
+  duration: 1,
+}, "<");
 tl.to("#yellow", {
-  y: -300,
-});
+  y: -370,
+  duration: 1,
+},"<");
+
+
+
+
+
+tl.to("#text", {
+  y: -150,
+  duration: 1,
+}, "<");
+
+
+
+
+
