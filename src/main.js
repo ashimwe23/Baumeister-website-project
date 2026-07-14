62ef33fa57a8790756e0c949f7d212e58897116c
Belyse Ashimwe <ashimwebelyse233@gmail.com>
2026-07-14 17:31:58 +0200

    Added the responsiveness on sm

--- src/main.js
+++ src/main.js
@@ -1,119 +1,137 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline({
+
+// Desktop Animationn
+
+const desktopTl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".hero",
+    trigger: ".desktop-hero",
     start: "top top",
     end: "+=1500",
     scrub: true,
-    pin: true,                                                                                                                                                 
+    pin: true, 
+    invalidateOnRefresh:true                                                                                                                                                
   },
 });
 
-tl.to("#pink", {
+desktopTl.to("#pink", {
   y: -19,
   duration: 0.2,
-}, "<");
+}, "<")
 
-tl.to("#text", {
+.to("#text-desktop", {
   y: -480,
   duration: 1,
-}, "<");
+}, "<")
 
-tl.to("#green2", {
+.to("#green2", {
   y: -190,
   duration: 1,
-},"<");
+},"<")
 
 
 
-tl.to(["#blue"], {
+.to(["#blue"], {
   y: -60,
   duration: 0.2,
-}, "<");
+}, "<")
 
 
-tl.to("#green", {
+.to("#green", {
   y: -105,
   duration: 0.3,
-}, "<");
+}, "<")
 
 
 
-tl.to("#red", {
+.to("#red", {
   y: -320,
   duration: 1.3,
-}, "<");
+}, "<")
 
-tl.to(["#green2"], {
+.to(["#green2"], {
   y: -370,
  duration: 0.2,
-}), "<";
+}, "<")
 
 
-tl.to("#yellow", {
+.to("#yellow", {
   y: -420,
   duration: 0.3,
-},"<");
+},"<")
 
 
 
-tl.to("#hero-cover", {
+.to("#hero-cover", {
   y: -180,
   duration: 1,
 }, "<");
 
 
-// The mobile hero section responsiveness
 
 
-tl.to("#pink-mobile", {
-  y: -350,
-  duration: 0.8,
+
+
+
+// // The mobile hero section Animation
+
+
+const mobileTl = gsap.timeline({
+  scrollTrigger:{
+    trigger: ".mobile-hero",
+    start:"top top",
+    end:"+=1000",
+    scrub:true,
+    pin:true,
+    invalidateOnRefresh:true
+  }
+});
+
+
+mobileTl.to("#pink-mobile", {
+ y:-55,
+ duration: 0.5,
+})
+
+.to("#text-mobile", {
+  y: -250,
+  duration: 1,
+}, "<")
+
+.to(["#blue-mobile","#green-mobile"],{
+ y:-80,
+ duration: 0.6,
+},"<")
+
+
+
+
+
+.to("#green2-mobile",{
+ y:-150,
+ duration: 1,
+},"<")
+
+
+.to("#red-mobile",{
+ y:-200,
+ duration: 1.2,
+}, "<")
+
+
+
+.to("#yellow-mobile",{
+ y:-100,
+ duration: 1.2,
+},)
+
+.to("#hero-cover-mobile", {
+  y: -227,
+  duration: 1,
 }, "<");
 
 
 
-tl.to("#green2-mobile", {
-  y: -380,
-  duration: 1,
-},"<");
-
-
-
-tl.to(["#blue-mobile", "#green-mobile"],{
-  y: -400,
-  duration: 1,
-}, "<");
-
-
-tl.to("#red-mobile", {
-  y: -500,
-  duration: 1,
-}, "<");
-
-
-
-tl.to("#yellow-mobile", {
-  y: -460,
-  duration: 1,
-},"<");
-
-tl.to(["#green2-mobile"], {
-  y: -480,
- duration: 0.8,
-}), "<";
-
-
-
-tl.to("#text-mobile", {
-  y: -480,
-  duration: 1,
-}, "<");
-
-
-
-tl.to("#hero-cover-mobile", {
-  y: -490,
-  duration: 1.5,
-}, "<");
+window.addEventListener("load", () => {
+  ScrollTrigger.refresh();
+});
